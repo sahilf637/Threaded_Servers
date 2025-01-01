5cfc293d11935769b231611faf5d0697c2140656
sahilf637 <sahilfartyal3@gmail.com>
2025-01-01 19:12:37 +0530

    threadPool

---
+++ Thread_Pool/Server.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+  private final ExecutorService threadPool;
+
+  public Server(int poolSize) {
+    this.threadPool = Executors.newFixedThreadPool(poolSize);
+  }
+
+  public void handleClient(Socket clientSocket) {
+    try (PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)) {
+      toSocket.println("Hello from server");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    int port = 8010;
+    int poolSize = 10;
+
+    Server server = new Server(poolSize);
+
+    try {
+      ServerSocket serverSocket = new ServerSocket(port);
+      serverSocket.setSoTimeout(70000);
+      System.out.println("Server started on port " + port);
+
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        server.threadPool.execute(() -> server.handleClient(clientSocket));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      server.threadPool.shutdown();
+    }
+  }
+
+}
